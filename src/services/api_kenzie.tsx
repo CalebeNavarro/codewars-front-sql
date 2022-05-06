1191674c8f36a7aaa44e9d9c3e042460d29a2bf0
CalebeNavarro <calebe.snavarro@gmail.com>
2022-05-06 17:51:12 -0300

    fix: back-end change the url

--- src/services/api_kenzie.tsx
+++ src/services/api_kenzie.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api_kenzie = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/v1",
+  baseURL: "https://codewars-kenzie-sql.herokuapp.com/api/v1",
 });
 
 export default api_kenzie;

a2ea885463251f0ec37025e36c3b93855fde7380
CalebeNavarro <calebe.snavarro@gmail.com>
2022-05-08 13:34:35 -0300

    feat: button tu delete account

--- src/components/UserPerfil/index.jsx
+++ src/components/UserPerfil/index.jsx
@@ -3,19 +3,40 @@ import { StudentInfo } from "../../providers/NameEnabler";
 import { EnablerInfo } from "../../providers/Enabler";
 import api_kenzie from "../../services/api_kenzie";
 
+
+import * as React from 'react';
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import Button from '@mui/material/Button';
-
-import SendIcon from '@mui/icons-material/Send';
 import LogoutIcon from '@mui/icons-material/Logout';
 import UpgradeIcon from '@mui/icons-material/Upgrade';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 import { toast } from 'react-toastify';
+
+import { useNavigate } from "react-router-dom";
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  color: 'black',
+  p: 4,
+};
+
 
 
 const schema = yup.object().shape({
@@ -29,11 +50,16 @@ const schema = yup.object().shape({
 const UserPerfil = () => {
   const { student, getInfoUser, token, setIsLogin } = StudentInfo();
   const { listEnabler } = EnablerInfo();
+  let navigate = useNavigate();
 
   const [ enabler, setEnabler ] = useState("");
   const [ update, setUpdate ] = useState(false);
   const [ name, setName ] = useState(student["user"]?.name)
   const [ username, setUsername ] = useState(student["user"]?.username)
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     findEnabler(student.enabler_id)
@@ -109,6 +135,25 @@ const UserPerfil = () => {
     setUpdate(false);
   }
 
+  const deleteAccount = async () => {
+    await api_kenzie.delete('/student', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      if(error.response) {
+        console.log(error.resopnse.data)
+      }
+    })
+    localStorage.clear();
+    setIsLogin(false);
+    return navigate("/");
+  }
+
   return (
     <UserProfileStyle>
       <h1>Welcome</h1>
@@ -160,8 +205,28 @@ const UserPerfil = () => {
           <p><span>Username:</span> {student["user"].username}</p>
         <p><span>Email:</span> {student["user"].email}</p>
         <p><span>Facilitador:</span> {enabler["user"]?.name}</p>
+
+
       </MainContainer>
       )}
+
+      
+      <Button onClick={handleOpen}>Delatar conta</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2" style={{color: "black"}}>
+              Você realmente deseja deletar a conta?
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <Button onClick={deleteAccount} variant="outlined" startIcon={<DeleteIcon />}>Sim, desejo deletar a conta!</Button>
+            </Typography>
+          </Box>
+        </Modal>
 
       <Button onClick={logOut} variant="contained" endIcon={<LogoutIcon />}>Logout</Button>
     </UserProfileStyle>
